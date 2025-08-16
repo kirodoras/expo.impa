f3b2b62dd673950858094b761eaeefb4fc8727d6
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2025-08-15 21:37:37 -0300

    feat: adicionar descrição detalhada da singularidade hiperbólica em ShowModels

--- src/ShowModels.jsx
+++ src/ShowModels.jsx
@@ -7,6 +7,31 @@ import MarkdownWithMath from "./MarkdownWithMath";
 
 const models = [
   {
+    file: "singularidade_hiperbolica.ply",
+    md: `### Singularidade hiperbólica
+Curva integral da EDO complexa
+$$
+\\left\\{
+\\begin{array}{rcl}
+\\dot x &=&-y+\\frac 1{10}x\\\\
+\\dot y &=&x+\\frac 1{10}y\\\\
+\\end{array}
+\\right.
+$$
+
+em $\\mathbb C^2$ passando pelo ponto $(1,1)$.
+
+As curvas integrais desta EDO são superfícies de Riemann que ilustram o comportamento na vizinhança de uma singularidade hiperbólica.
+
+Para ser visualizada, esta superfície foi projetada ortogonalmente no espaço euclidiano tridimensional
+$$
+\\mathrm{Re}(x) \\times \\mathrm{Im}(x) \\times \\mathrm{Re}(y)
+$$
+
+Pontos de cores diferentes indicam que são pontos com coordenada $\\mathrm{Im}(x)$ diferentes.`,
+    title: "Singularidade hiperbólica",
+  },
+  {
     file: "genero2.ply",
     md: `
 ### Curva algébrica de gênero 2
@@ -169,31 +194,6 @@ $$
 
 Pontos de cores diferentes indicam que são pontos com coordenada $\\mathrm{Im}(x)$ diferentes.`,
     title: "Folheação de Vander Pol",
-  },
-  {
-    file: "singularidade_hiperbolica.ply",
-    md: `### Singularidade hiperbólica
-Curva integral da EDO complexa
-$$
-\\left\\{
-\\begin{array}{rcl}
-\\dot x &=&-y+\\frac 1{10}x\\\\
-\\dot y &=&x+\\frac 1{10}y\\\\
-\\end{array}
-\\right.
-$$
-
-em $\\mathbb C^2$ passando pelo ponto $(1,1)$.
-
-As curvas integrais desta EDO são superfícies de Riemann que ilustram o comportamento na vizinhança de uma singularidade hiperbólica.
-
-Para ser visualizada, esta superfície foi projetada ortogonalmente no espaço euclidiano tridimensional
-$$
-\\mathrm{Re}(x) \\times \\mathrm{Im}(x) \\times \\mathrm{Re}(y)
-$$
-
-Pontos de cores diferentes indicam que são pontos com coordenada $\\mathrm{Im}(x)$ diferentes.`,
-    title: "Singularidade hiperbólica",
   },
 ];
 
