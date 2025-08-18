e4309a41eb72b8fef32e5c3e15fc0151eadc3c0a
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2025-08-18 18:57:21 -0300

    Add new model for Cúbica III with detailed mathematical description and visualization notes

--- src/ShowModels.jsx
+++ src/ShowModels.jsx
@@ -124,6 +124,42 @@ Pontos de cores diferentes indicam que são pontos com coordenada $\\mathrm{Im}(
     title: "Cúbica II",
   },
   {
+    file: "cubica_III.ply",
+    md: `### Cúbica III
+Curva integral da EDO complexa
+$$
+\\left\\{
+\\begin{array}{rcl}
+\\dot x &=&x^2+2xy-2y^2-x+2y\\\\
+\\dot y &=&2x^2-2xy-y^2-2x+y
+\\end{array}
+\\right.
+$$
+
+em $\\mathbb C^2$ passando pelo ponto $\\left(-\\dfrac 15,-\\dfrac 15\\right)$.
+
+As curvas integrais desta EDO são, genericamente, curvas algébricas de gênero $1$ com $3$ pontos no infinito, definidas pela equação
+$$
+(x+y-1)(2x-y+1)(x-2y-1) = \\mathrm{cte}
+$$
+
+A ilustração é do toro
+$$
+(x+y-1)(2x-y+1)(x-2y-1)=-\\dfrac{112}{125}
+$$
+subtraído de três discos.
+
+Para ser visualizada, esta superfície foi projetada ortogonalmente no espaço euclidiano tridimensional
+$$
+\\mathrm{Re}(x) \\times \\mathrm{Im}(x) \\times \\left(\\dfrac{- {\\mathrm{Re}}(y) + {\\mathrm{Im}}(y)}{\\sqrt{2}}\\right)
+$$
+
+Pontos de cores diferentes indicam que são pontos com coordenada $\\dfrac{{\\mathrm{Re}}(y)+ {\\mathrm{Im}}(y)}{\\sqrt 2}$ diferentes.
+
+Essa é mesma ilustração da **Cúbica II** (ilustração anterior) mas, em uma projeção tridimensional diferente.`,
+    title: "Cúbica III",
+  },
+  {
     file: "cilindro_I.ply",
     md: `### Cilindro I
 Curva integral da EDO complexa
